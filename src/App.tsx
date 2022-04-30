971f0a9eb87d2b19f422c3352f5e44952ab0f600
hydra13 <rob.sabirov@yandex.ru>
2022-04-30 11:15:55 +0300

    fix tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import {Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Header} from "./components";
 import {About, Info, Main, NotFound} from './pages';
 
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="main" element={<Main />} />
-        <Route path="about" element={<About />} />
-        <Route path="info" element={<Info />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="main" element={<Main />} />
+          <Route path="about" element={<About />} />
+          <Route path="info" element={<Info />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
